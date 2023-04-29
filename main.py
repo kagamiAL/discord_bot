0abb651762243800da352b8302780fd0c48ae0d2
Alan <kagamibach@gmail.com>
2023-04-29 16:18:26 -0600

    more security to muting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import discord
 import asyncio
-from global_data import print_report
+from global_data import *
 from discord.ext import commands;
 import os
 from dotenv import load_dotenv
@@ -8,7 +8,14 @@ from dotenv import load_dotenv
 intents = discord.Intents.all()
 bot     = commands.Bot(command_prefix='#', intents=intents);
 
+#--global functions
 
+async def delete_messages_if_muted(message: discord.Message):
+    if (get_server_data(message.guild.id).is_currently_muted(message.author)):
+        await message.delete()
+    return;
+
+#--bot events
 @bot.event
 async def on_ready():
     print("Logged in as {0.user}".format(bot));
@@ -20,6 +27,7 @@ async def on_message(message):
     user_message: str = message.content;
     print_report(f'User {message.author} sent a message: {user_message}')
     await bot.process_commands(message)
+    await delete_messages_if_muted(message)
 
 load_dotenv(encoding='utf-16')
 async def main():
